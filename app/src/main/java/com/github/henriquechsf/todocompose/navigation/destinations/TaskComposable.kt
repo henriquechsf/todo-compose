003bba807b6cb3cf7cf2a0a0a3789f92fa832224
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-02 16:01:10 -0300

    display dynamic task content

--- app/src/main/java/com/github/henriquechsf/todocompose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/github/henriquechsf/todocompose/navigation/destinations/TaskComposable.kt
@@ -1,5 +1,6 @@
 package com.github.henriquechsf.todocompose.navigation.destinations
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavGraphBuilder
@@ -26,8 +27,13 @@ fun NavGraphBuilder.taskComposable(
         val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
         sharedViewModel.getSelectedTask(taskId)
         val selectedTask by sharedViewModel.selectedTask.collectAsState()
+        
+        LaunchedEffect(key1 = taskId) {
+            sharedViewModel.updateTaskFields(selectedTask = selectedTask)
+        }
 
         TaskScreen(
+            sharedViewModel = sharedViewModel,
             selectedTask = selectedTask,
             navigateToListScreen = navigateToListScreen
         )
--- app/src/main/java/com/github/henriquechsf/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/github/henriquechsf/todocompose/ui/screens/task/TaskScreen.kt
@@ -3,16 +3,24 @@ package com.github.henriquechsf.todocompose.ui.screens.task
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.github.henriquechsf.todocompose.data.models.Priority
 import com.github.henriquechsf.todocompose.data.models.TodoTask
+import com.github.henriquechsf.todocompose.ui.viewmodels.SharedViewModel
 import com.github.henriquechsf.todocompose.util.Action
 
 @Composable
 fun TaskScreen(
+    sharedViewModel: SharedViewModel,
     selectedTask: TodoTask?,
     navigateToListScreen: (Action) -> Unit,
 ) {
+
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority: Priority by sharedViewModel.priority
+
     Scaffold(
         topBar = {
             TaskAppBar(
@@ -20,15 +28,21 @@ fun TaskScreen(
                 navigateToListScreen = navigateToListScreen
             )
         },
-        content = {
+        content = { it ->
             TaskContent(
                 modifier = Modifier.padding(it),
-                title = "",
-                onTitleChange = {},
-                description = "",
-                onDescriptionChange = {},
-                priority = Priority.LOW,
-                onPrioritySelected = {}
+                title = title,
+                onTitleChange = {
+                    sharedViewModel.title.value = it
+                },
+                description = description,
+                onDescriptionChange = {
+                    sharedViewModel.description.value = it
+                },
+                priority = priority,
+                onPrioritySelected = {
+                    sharedViewModel.priority.value = it
+                }
             )
         }
     )
