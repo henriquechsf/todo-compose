6748a814e997380c37b1ca6a1589851d3b9075c6
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-02 12:30:15 -0300

    new task app bar UI

---
+++ app/src/main/java/com/github/henriquechsf/todocompose/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,21 @@
+package com.github.henriquechsf.todocompose.ui.screens.task
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TaskScreen() {
+    Scaffold(
+        topBar = {
+
+        },
+        content = {
+            Column(modifier = Modifier.padding(it)) {
+
+            }
+        }
+    )
+}
