1af4ded531083a7d29a355910ca3ac79631dc968
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-03 16:07:35 -0300

    fix add same task on back button

--- app/src/main/java/com/github/henriquechsf/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/github/henriquechsf/todocompose/ui/screens/task/TaskScreen.kt
@@ -2,10 +2,12 @@ package com.github.henriquechsf.todocompose.ui.screens.task
 
 import android.content.Context
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.github.henriquechsf.todocompose.data.models.Priority
@@ -25,6 +27,8 @@ fun TaskScreen(
     val priority: Priority by sharedViewModel.priority
 
     val context = LocalContext.current
+
+    BackHandler(onBackPressed = { navigateToListScreen(Action.NO_ACTION) })
 
     Scaffold(
         topBar = {
@@ -65,4 +69,29 @@ fun TaskScreen(
 
 fun displayToast(context: Context) {
     Toast.makeText(context, "Fields Empty.", Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun BackHandler(
+    backDispatcher: OnBackPressedDispatcher? =
+        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit,
+) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+
+    val backCallback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+    
+    DisposableEffect(key1 = backDispatcher) {
+        backDispatcher?.addCallback(backCallback)
+
+        onDispose {
+            backCallback.remove()
+        }
+    }
 }
