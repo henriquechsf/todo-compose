adf2269dd30f7cd98d07b815385b005846f3d4de
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-03 12:04:45 -0300

    animate item visibility

--- app/src/main/java/com/github/henriquechsf/todocompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/github/henriquechsf/todocompose/ui/screens/list/ListContent.kt
@@ -1,6 +1,10 @@
 package com.github.henriquechsf.todocompose.ui.screens.list
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -9,8 +13,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -20,6 +23,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.henriquechsf.todocompose.R
 import com.github.henriquechsf.todocompose.data.models.Priority
 import com.github.henriquechsf.todocompose.data.models.TodoTask
@@ -120,18 +124,33 @@ fun DisplayTasks(
                 targetValue = if (dismissState.targetValue == DismissValue.Default) 0f else -45f
             )
 
-            SwipeToDismiss(
-                state = dismissState,
-                directions = setOf(DismissDirection.EndToStart),
-                dismissThresholds = { FractionalThreshold(fraction = 0.7f) },
-                background = { RedBackground(degrees = degrees) },
-                dismissContent = {
-                    TaskItem(
-                        todoTask = task,
-                        navigateToTaskScreen = navigateToTaskScreen
-                    )
-                }
-            )
+            var itemAppeared by remember { mutableStateOf(false) }
+            LaunchedEffect(key1 = true) {
+                itemAppeared = true
+            }
+
+            AnimatedVisibility(
+                visible = itemAppeared && !isDismissed,
+                enter = expandVertically(
+                    animationSpec = tween(durationMillis = 300)
+                ),
+                exit = shrinkVertically(
+                    animationSpec = tween(durationMillis = 300)
+                )
+            ) {
+                SwipeToDismiss(
+                    state = dismissState,
+                    directions = setOf(DismissDirection.EndToStart),
+                    dismissThresholds = { FractionalThreshold(fraction = 0.7f) },
+                    background = { RedBackground(degrees = degrees) },
+                    dismissContent = {
+                        TaskItem(
+                            todoTask = task,
+                            navigateToTaskScreen = navigateToTaskScreen
+                        )
+                    }
+                )
+            }
         }
     }
 }
@@ -219,4 +238,12 @@ fun TaskItemPreview() {
         ),
         navigateToTaskScreen = {}
     )
+}
+
+@Preview
+@Composable
+fun RedBackgroundPreview() {
+    Column(modifier = Modifier.height(98.dp)) {
+        RedBackground(degrees = 0f)
+    }
 }
