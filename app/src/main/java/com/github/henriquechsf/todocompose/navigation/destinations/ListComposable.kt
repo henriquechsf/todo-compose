b68dfe180e351470a4b4140fd991a9eb6c74e246
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-03 17:17:05 -0300

    fix action task on rotate screen

--- app/src/main/java/com/github/henriquechsf/todocompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/github/henriquechsf/todocompose/navigation/destinations/ListComposable.kt
@@ -3,12 +3,16 @@ package com.github.henriquechsf.todocompose.navigation.destinations
 import android.util.Log
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.github.henriquechsf.todocompose.ui.screens.list.ListScreen
 import com.github.henriquechsf.todocompose.ui.viewmodels.SharedViewModel
+import com.github.henriquechsf.todocompose.util.Action
 import com.github.henriquechsf.todocompose.util.Constants.LIST_ARGUMENT_KEY
 import com.github.henriquechsf.todocompose.util.Constants.LIST_SCREEN
 import com.github.henriquechsf.todocompose.util.toAction
@@ -27,8 +31,13 @@ fun NavGraphBuilder.listComposable(
 
         val action = navBackStackEntry.arguments?.getString(LIST_ARGUMENT_KEY).toAction()
 
+        var myAction by rememberSaveable { mutableStateOf(Action.NO_ACTION) }
+
         LaunchedEffect(key1 = action) {
-            sharedViewModel.action.value = action
+            if (action != myAction) {
+                myAction = action
+                sharedViewModel.action.value = action
+            }
         }
 
         val databaseAction by sharedViewModel.action
